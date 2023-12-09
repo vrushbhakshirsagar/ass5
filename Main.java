2a746fe16bffa22400d3c98166b70e44cd7d5dec
vrushbhakshirsagar <rishbhakshirsagar@gmail.com>
2023-12-09 21:30:54 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("Hello Vrushabha!");
+        System.out.println("by vrushbha!");
+    }
+}
+    
+    
+
